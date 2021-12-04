ea469241f5d2ba49872816a586543a3d4f64af44
Siddharth.Patel <opensiddhu993@gmail.com>
2021-12-04 18:30:40 +0530

    local tunnel API inserted

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
-// export const api='https://utopianbuys.loca.lt/api'
-export const api='http://localhost:2000/api'
+export const api='https://utopianbuys.loca.lt/api'
+// export const api='http://localhost:2000/api'
 // export const api='https://utopianbuys.herokuapp.com/api/'
 export const generatePublicUrl=(filename)=>{
     // return `http://localhost:2000/public/${filename}`
