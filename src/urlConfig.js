90b7deee0b9c2cc4eae25145f7e9b114e9638ac4
Siddharth.Patel <opensiddhu993@gmail.com>
2021-12-08 16:56:44 +0530

    netlify heroku and atlas try

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,8 @@
-export const api='https://utopianbuys.loca.lt/api'
+// export const api='https://utopianbuys.loca.lt/api'
 // export const api='http://localhost:2000/api'
-// export const api='https://utopianbuys.herokuapp.com/api/'
-const photoAPI='https://utopianbuys.loca.lt/public'
+export const api='https://utopianbuys.herokuapp.com/api/'
+// const photoAPI='https://utopianbuys.loca.lt/public'
+const photoAPI='https://utopianbuys.s3.ap-south-1.amazonaws.com/uploads'
 export const generatePublicUrl=(filename)=>{
     // return `http://localhost:2000/public/${filename}`
     return `${filename}`
@@ -12,6 +13,14 @@ export const localImageToLocalTunnelImage = (localLink) => {
     if (localLink) {
         const myArray = localLink.split("http://localhost:2000/public")[1];
         const LocalTunnelLink=photoAPI+myArray;
-        return LocalTunnelLink   
+        return LocalTunnelLink; 
     }
 }
+
+// export const localImageToLocalTunnelImage = (localLink) => {
+//     if (localLink) {
+//         const myArray = localLink.split("http://localhost:2000/public")[1];
+//         const LocalTunnelLink=photoAPI+myArray;
+//         return LocalTunnelLink   
+//     }
+// }
